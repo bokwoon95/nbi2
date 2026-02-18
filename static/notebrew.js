9c81ff26ba2a265a3e65497c08325139eb10b52c
bokwoon <bokwoon.c@gmail.com>
2026-02-18 23:52:47 +0800

    checkpoint

--- static/notebrew.js
+++ static/notebrew.js
@@ -2,26 +2,73 @@ import "./basecoat.js";
 import "./sidebar.js";
 import "./dropdown-menu-v4.js";
 
-for (const element of document.querySelectorAll("[data-toggle-sidebar]")) {
-  element.addEventListener("click", function() {
-    document.dispatchEvent(new Event('basecoat:sidebar'));
-  });
-}
-
-for (const element of document.querySelectorAll("[data-go-back]")) {
-  if (element.tagName != "A") {
-    continue;
-  }
-  element.addEventListener("click", function goBack(event) {
-    if (!(event instanceof PointerEvent)) {
+/**
+ * @type {Record<string, (element: Element, attributeValue: string) => void>}
+ */
+const initFunctions = {
+  "data-click-event": function initClickEvent(targetElement, attributeValue) {
+    targetElement.addEventListener("click", function dispatchEvent() {
+      document.dispatchEvent(new Event(attributeValue));
+    });
+  },
+  "data-go-back": function initGoBack(targetElement) {
+    if (targetElement.tagName != "A") {
       return;
     }
-    if (document.referrer && history.length > 2 && !event.ctrlKey && !event.metaKey) {
-      event.preventDefault();
-      history.back();
+    targetElement.addEventListener("click", function goBack(event) {
+      if (!(event instanceof PointerEvent)) {
+        return;
+      }
+      if (document.referrer && history.length > 2 && !event.ctrlKey && !event.metaKey) {
+        event.preventDefault();
+        history.back();
+      }
+    });
+  },
+};
+const attributeNames = Object.keys(initFunctions);
+/**
+ * @param {Element} targetElement
+ */
+function initialize(targetElement) {
+  for (const attributeName of attributeNames) {
+    if (targetElement.hasAttribute(attributeName) && !targetElement.hasAttribute(attributeName + "-initialized")) {
+      try {
+        initFunctions[attributeName](targetElement, targetElement.getAttribute(attributeName));
+      } catch (e) {
+        console.error(e);
+      }
+      targetElement.setAttribute(attributeName + "-initialized", "");
     }
-  });
+  }
 }
+const selector = attributeNames.map(name => "[" + name + "]").join(", ");
+for (const targetElement of document.querySelectorAll(selector)) {
+  initialize(targetElement);
+}
+const observer = new MutationObserver(function(mutationRecords) {
+  for (const mutationRecord of mutationRecords) {
+    if (mutationRecord.type != "childList") {
+      continue;
+    }
+    for (const addedElement of mutationRecord.addedNodes) {
+      if (!(addedElement instanceof Element)) {
+        continue;
+      }
+      initialize(addedElement);
+      for (const targetElement of targetElement.querySelectorAll(selector)) {
+        if (!(targetElement instanceof Element)) {
+          continue;
+        }
+        initialize(targetElement);
+      }
+    }
+  }
+});
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
 
 function humanReadableFileSize(size) {
   if (size < 0) {
